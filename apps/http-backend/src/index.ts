fc4c6440caf3ef8c457a2db104670ad825a457a0
Utsav Singh <utsav0111@gmail.com>
2026-07-17 02:08:50 +0530

    improved http backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,91 +1,95 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET }from "@repo/backend-common/config"; 
-import {middleware} from "./middleware";
-import { createUserSchema , signinSchema , createRoomSchema } from "@repo/common/types";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { middleware } from "./middleware";
+import { createUserSchema, signinSchema, createRoomSchema } from "@repo/common/types";
 import { prismaClient } from "@repo/db/client"
 
 type AuthRequest = Request & {
-  userId?: string;
+    userId?: string;
 };
 
 const app = express();
 
-app.post("/signup",async (req,res)=>{
-    const data = createUserSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/signup", async (req, res) => {
+    const parsedData = createUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
         return res.status(400).json({
-            message:"Incorrect inputs"
+            message: "Incorrect inputs"
         })
-         
-    }
-    const user = await prismaClient.user.create({
-        data:{
-            username:data.data.username,
-            password:data.data.password,
-            name:data.data.name,
-            email:"todo@example.com"
-        },
 
-    });
-    
-    res.json({
-        userId:user.id,
-    });
+    }
+    try {
+        const user = await prismaClient.user.create({
+            data: {
+                email: parsedData.data.email,
+                password: parsedData.data.password,
+                name: parsedData.data.name,
+            },
+        });
+        res.json({
+            userId: user.id,
+        });
+    } catch (e) {
+        res.status(404).json(
+            {
+                message: "user already exists"
+            }
+        )
+    }
 
 });
-app.post("/signin",async(req,res)=>{
-    const data = signinSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/signin", async (req, res) => {
+    const parsedData = signinSchema.safeParse(req.body);
+    if (!parsedData.success) {
         return res.status(400).json({
-            message:"incorrect input"
+            message: "incorrect input"
         });
-        
+
     }
     const user = await prismaClient.user.findFirst({
-        where:{
-            username:data.data.username,
-            password:data.data.password,
+        where: {
+            email: parsedData.data.email,
+            password: parsedData.data.password,
         },
     });
-    if(!user){
+    if (!user) {
         return res.status(403).json({
-            message:"user not found"
+            message: "user not found"
         });
     }
     const token = jwt.sign({
         userId: user.id
-    },JWT_SECRET);
+    }, JWT_SECRET);
     res.json({
         token
     });
 
 });
-app.post("/room",middleware,async(req:AuthRequest,res:Response)=>{
-    const data = createRoomSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/room", middleware, async (req: AuthRequest, res: Response) => {
+    const parsedData = createRoomSchema.safeParse(req.body);
+    if (!parsedData.success) {
         return res.status(400).json({
-            message:"incorrent input"
-        });   
+            message: "incorrent input"
+        });
     }
     const userId = req.userId;
     if (!userId) {
-    return res.status(403).json({
-      message: "user not found",
-    });
+        return res.status(403).json({
+            message: "user not found",
+        });
     }
     const room = await prismaClient.room.create({
-        data:{
-            name:data.data.name,
-            slug:data.data.name.toLowerCase().replace(/\s+/g, "-"),
-            adminId:userId,
+        data: {
+            slug: parsedData.data.name.toLowerCase().replace(/\s+/g, "-"),
+            adminId: userId,
         }
     })
 
     res.json({
-        roomId:room.id,
+        roomId: room.id,
     });
-     
+
 });
 
 app.listen(3001);
