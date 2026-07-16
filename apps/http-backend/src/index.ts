759f15a7043b3321b013ebad221531a39e72730c
Utsav Singh <utsav0111@gmail.com>
2026-07-17 01:21:08 +0530

    add the db calls in http backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,56 +1,91 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET }from "@repo/backend-common/config"; 
 import {middleware} from "./middleware";
 import { createUserSchema , signinSchema , createRoomSchema } from "@repo/common/types";
+import { prismaClient } from "@repo/db/client"
+
+type AuthRequest = Request & {
+  userId?: string;
+};
 
 const app = express();
 
-app.post("/signup",(req,res)=>{
+app.post("/signup",async (req,res)=>{
     const data = createUserSchema.safeParse(req.body);
     if(!data.success){
-        res.json({
+        return res.status(400).json({
             message:"Incorrect inputs"
         })
-        return ; 
+         
     }
-    //db call 
-    res.json({
-        userId:123 
-    })
+    const user = await prismaClient.user.create({
+        data:{
+            username:data.data.username,
+            password:data.data.password,
+            name:data.data.name,
+            email:"todo@example.com"
+        },
 
-})
-app.post("/signin",(req,res)=>{
+    });
+    
+    res.json({
+        userId:user.id,
+    });
+
+});
+app.post("/signin",async(req,res)=>{
     const data = signinSchema.safeParse(req.body);
     if(!data.success){
-        res.json({
+        return res.status(400).json({
             message:"incorrect input"
-        })
-        return;
+        });
+        
     }
-    const userId = 1;
+    const user = await prismaClient.user.findFirst({
+        where:{
+            username:data.data.username,
+            password:data.data.password,
+        },
+    });
+    if(!user){
+        return res.status(403).json({
+            message:"user not found"
+        });
+    }
     const token = jwt.sign({
-        userId
+        userId: user.id
     },JWT_SECRET);
     res.json({
         token
-    })
+    });
 
-})
-app.post("/room",middleware,(req,res)=>{
+});
+app.post("/room",middleware,async(req:AuthRequest,res:Response)=>{
     const data = createRoomSchema.safeParse(req.body);
     if(!data.success){
-        res.json({
+        return res.status(400).json({
             message:"incorrent input"
-        })
-        return;
+        });   
     }
-    //db call
+    const userId = req.userId;
+    if (!userId) {
+    return res.status(403).json({
+      message: "user not found",
+    });
+    }
+    const room = await prismaClient.room.create({
+        data:{
+            name:data.data.name,
+            slug:data.data.name.toLowerCase().replace(/\s+/g, "-"),
+            adminId:userId,
+        }
+    })
 
     res.json({
-        roomId:123
-    })
+        roomId:room.id,
+    });
      
-})
+});
 
 app.listen(3001);
